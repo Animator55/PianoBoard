f8f686efc44499173e9334decb87b9b17f6507c7
Animator55 <92542799+Animator55@users.noreply.github.com>
2024-02-08 12:44:31 -0300

    cleaned code

--- src/App.tsx
+++ src/App.tsx
@@ -1,236 +1,66 @@
 import React from 'react'
 import './assets/App.css'
 import PianoKey from './components/PianoKey'
-import { CachedRecords, RecordedKey, configuration } from './vite-env'
-// import RenderCached from './components/RenderCached'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck'
-import { 
-  // faCircleDot, 
-  // faCircleStop, 
-  faGear
-} from '@fortawesome/free-solid-svg-icons'
+import {
+  configuration
+} from './vite-env'
 import VolumeButton from './components/VolumeButton'
-
-let Record : RecordedKey[] = []
-const setRecord = (val: RecordedKey[]) =>{
-  Record = val
-}
 
 const defaultconfig = {
   viewBinds: true,
   viewNotes: true,
   volume: 0.25,
-  keyBinds:{
-    "1.0": "a",
-    "1.1": "w",
-    "1.2": "s",
-    "1.3": "e",
-    "1.4": "d",
-    "1.5": "f",
-    "1.6": "t",
-    "1.7": "g",
-    "1.8": "y",
-    "1.9": "h",
-    "1.10": "u",
-    "1.11": "j",
-  }
+  keyBinds: {}
 }
-
-// let PlayingRecords: string[] = []
-
-// const detectRecordsIndex = (id: string): number=>{
-//   let index: number = -1
-//   for(let i=0;i<PlayingRecords.length; i++){
-//     let entry: string = PlayingRecords[i]
-//     let EntryId = entry.split("-")[0]
-//     if(id === EntryId) {
-//       index = i
-//       break
-//     }
-//   }
-
-//   return index
-// }
 
 export default function App() {
   const [octaves, setOctaves] = React.useState<number>(2)
-  const [recording, startRecording] = React.useState<boolean>(false)
-  const [editing, startEditing] = React.useState<boolean>(false)
   const [configuration, setConfiguration] = React.useState<configuration>(defaultconfig)
-  const [CachedRecords, setCachedRecords] = React.useState<CachedRecords>({})
   const BoardRef = React.useRef<HTMLDivElement | null>(null)
 
-  //functions
-
-  // const recordKey = (octave:number | undefined, index:number| undefined, timestamp:number)=>{
-  //   if(!recording) return 
-  //   if(Record.length === 0) {
-  //     let Recorded: RecordedKey = {octave: octave, key: index, timestamp: 0, initialstamp: timestamp}
-  //     setRecord([...Record, Recorded])
-  //   }
-  //   else {
-  //     let Recorded: RecordedKey = {octave: octave, key: index, timestamp: timestamp - Record[0].initialstamp!}
-  //     let SilenceGap: RecordedKey = {octave: -1, key: -1, timestamp: Record[Record.length-1].timestamp, hold: timestamp - Record[0].initialstamp! - Record[Record.length-1].timestamp}
-  //     setRecord([...Record, SilenceGap, Recorded])
-  //   }
-  // }
-
-  // const StopRecord = (timeStamp:number, index: string, verifiyer: number)=>{
-  //   setTimeout(()=>{
-  //     if(!PlayingRecords.includes(index+"-"+verifiyer)||!BoardRef.current) return
-  //     PlayingRecords.splice(detectRecordsIndex(index), 1)
-  //     let recordButton: HTMLElement | null = document.getElementById(index)
-  //     if(recordButton !== null) {
-  //       let record = recordButton.firstChild as HTMLDivElement
-  //       record.style.transition = "none"
-  //       recordButton.classList.remove("playing")
-  //     }
-  //   }, timeStamp)
-  // }
-
-  // const playRecord = (index: string, playStop: boolean)=>{
-  //   const currentRecord = CachedRecords[index]
-  //   const verifiyer = Math.random()
-  //   if(playStop) PlayingRecords.push(index+"-"+verifiyer)
-  //   else PlayingRecords.splice(detectRecordsIndex(index), 1)
-
-  //   for(let i = 0;i<currentRecord.length;i++) {
-  //     let key: RecordedKey = currentRecord[i]
-  //     setTimeout(()=>{
-  //       if(!PlayingRecords.includes(index+"-"+verifiyer)||!BoardRef.current || key.key === undefined) return
-  //       if(currentRecord.length-1 === i) StopRecord(key.hold, index, verifiyer)
-  //       if(key.key !== -1) {
-  //         let keyIndex = key.key + 12 * (key.octave!-1)
-  //         const KeyElement = BoardRef.current.children[keyIndex] as HTMLButtonElement
-  //         KeyElement.classList.add("active")
-  //         setTimeout(()=>{
-  //           KeyElement.click()
-  //           KeyElement.classList.remove("active")
-  //         },100)
-  //       }
-  //     }, key.timestamp)
-  //   }
-  // }
-
-  // const newBind = (bind: string, key: string)=>{
-  //   setConfiguration({...configuration, keyBinds: {...configuration.keyBinds, [key]: bind}})
-  // }
-
-  const createID = () : string=>{
-    let id:string = `${Math.round(Math.random()*1000)}`
-    let idArray = Object.keys(CachedRecords)
-    while(idArray.includes(id)){
-      id = `${Math.round(Math.random()*1000)}`
-    }
-    return id
-  }
-
-  //components
-
-  const GenerateBoard = () =>{
-    let array : JSX.Element[] = []
-    for(let i=1; i<=octaves;i++){
-      for(let j=0;j<12;j++){
+  const GenerateBoard = () => {
+    let array: JSX.Element[] = []
+    for (let i = 1; i <= octaves; i++) {
+      for (let j = 0; j < 12; j++) {
         array.push(
           <PianoKey
-            // record={(timestamp:number)=>{recordKey(i, j, timestamp)}} 
-            key={Math.random()} 
-            octave={i} 
+            key={Math.random()}
+            octave={i}
             index={j}
             configuration={configuration}
-            // newBind={newBind}
-            editing={editing}
           />)
       }
     }
 
-    // const keyDown = (e: React.KeyboardEvent)=>{
-    //   let index = Object.values(configuration.keyBinds).indexOf(e.key.toLowerCase())
-    //   if(index === -1 || !BoardRef.current || editing) return
-    //   let [octave, key] = Object.keys(configuration.keyBinds)[index].split(".")
-    //   if(Number(octave) > octaves) return
-    //   let keyIndex: number = Number(key) + 12 * (Number(octave)-1)
-    //   const KeyElement = BoardRef.current.children[keyIndex] as HTMLButtonElement
-    //   KeyElement.classList.add("active")
-    //   setTimeout(()=>{
-    //     KeyElement.click()
-    //     KeyElement.classList.remove("active")
-    //   },100)
-    // }
-
-    return <section 
-        className='board' 
-        data-editing={editing ? "editing" : ""}
-        ref={BoardRef}
-        // onKeyDown={keyDown}
+    return <section
+      className='board'
+      ref={BoardRef}
     >{array}</section>
   }
 
-  const TopBar = ()=>{
-    // const RecordButton = ()=>{
-    //   let className = ""
-
-    //   className += editing ? "disabled ":""
-    //   className += recording ? "recording ":""
-
-    //   return <button className={className} onClick={(e:React.MouseEvent)=>{
-    //     if(editing) return
-    //     if(recording && Record.length !== 0) {
-    //       let SilenceGap: RecordedKey = {octave: -1, key: -1, timestamp: Record[Record.length-1].timestamp, hold: e.timeStamp - Record[0].initialstamp! - Record[Record.length-1].timestamp}
-    //       setRecord([...Record, SilenceGap])
-    //     }
-    //     startRecording(!recording)
-    //   }}><FontAwesomeIcon icon={recording ? faCircleStop : faCircleDot}/>REC</button>
-    // }
-
+  const TopBar = () => {
     return <nav className='top-bar'>
-      {/* <RecordButton/> */}
-      <select defaultValue={octaves} onChange={(e)=>{setOctaves(Number(e.currentTarget.value))}}>
+      <select defaultValue={octaves} onChange={(e) => { setOctaves(Number(e.currentTarget.value)) }}>
         <option value={1}>1 Octave</option>
         <option value={2}>2 Octave</option>
         <option value={3}>3 Octave</option>
       </select>
-      {/* <button style={{opacity: configuration.viewBinds ? 1:0.5}} onClick={()=>{setConfiguration({...configuration, viewBinds: !configuration.viewBinds})}}>
-        Binds
-      </button> */}
-      <button style={{opacity: configuration.viewNotes ? 1:0.5}} onClick={()=>{setConfiguration({...configuration, viewNotes: !configuration.viewNotes})}}>
+      <button style={{ opacity: configuration.viewNotes ? 1 : 0.5 }} onClick={() => { setConfiguration({ ...configuration, viewNotes: !configuration.viewNotes }) }}>
         Notes
       </button>
-      <VolumeButton volume={configuration.volume} changeVolume={(vol:number)=>{setConfiguration({...configuration, volume: vol})}}/>
-      <button className={editing ? 'config active':'config'} title="Configurate KeyBinds" onClick={()=>{startEditing(!editing)}}>
-        <FontAwesomeIcon icon={editing ? faCheck : faGear}/>
-      </button>
+      <VolumeButton volume={configuration.volume} changeVolume={(vol: number) => { setConfiguration({ ...configuration, volume: vol }) }} />
     </nav>
   }
 
-  //useEffect
-
-  React.useEffect(()=>{
-    if(!recording && Record.length !== 0) {
-      setCachedRecords({...CachedRecords, [createID()]: Record})
-      setRecord([])
-    }
-  }, [recording])
-
-  React.useEffect(()=>{
-    if(BoardRef.current) {
+  React.useEffect(() => {
+    if (BoardRef.current) {
       let button = BoardRef.current.children[0] as HTMLButtonElement
       button.focus()
     }
   })
 
-  React.useEffect(()=>{
-    if(editing) {
-      setRecord([])
-      startRecording(false)
-      // PlayingRecords = []
-    }
-  }, [editing])
-
   return <div className='main'>
-    <TopBar/>
-    {/* <RenderCached CachedRecords={CachedRecords} playRecord={playRecord} editing={editing}/> */}
-    <GenerateBoard/>
+    <TopBar />
+    <GenerateBoard />
   </div>
 }
--- src/components/PianoKey.tsx
+++ src/components/PianoKey.tsx
@@ -4,10 +4,7 @@ import { configuration } from "../vite-env"
 type Props = {
     octave: number
     index: number
-    // record: Function
-    // newBind: Function
     configuration: configuration
-    editing: boolean
 }
 
 const MelodicalScale = ["Do", "Do#", "Re", "Re#", "Mi", "Fa","Fa#", "Sol", "Sol#", "La", "La#", "Si"]
@@ -16,32 +13,18 @@ let preventConstant = false
 
 
 export default function PianoKey({octave, index, 
-    // record, 
-    editing, configuration, 
-    // newBind
+    configuration, 
 }: Props) {
 
-    const PlaySound = (e: React.MouseEvent | React.KeyboardEvent)=>{
+    const PlaySound = (
+        )=>{
         if(preventConstant) return
-        console.log("played " + octave + ", " + MelodicalScale[index], e.timeStamp)
         PlaySoundMp3(octave, index, configuration.volume)
-        // record(e.timeStamp)
     }
-
-    // const EditKey = (e: React.MouseEvent)=>{
-    //     const detectKey = (e2: KeyboardEvent)=>{
-    //         if(e2.key === undefined) return
-    //         let compiledIndex = octave +"."+index
-    //         newBind(e2.key.toLowerCase(), compiledIndex)
-    //     }
-    //     e.currentTarget.classList.add("editing")
-    //     e.currentTarget.addEventListener("keydown", (e)=>{detectKey(e as KeyboardEvent)}, {once: true})
-    // }
 
     const checkBlack = (value: number)=>{
         let result = ""  
         result = MelodicalScale[value].slice(-1) === "#" ? "key-black" : "key-white"
-        result += editing ? " disabled-key" : ""
         return result
     }
 
@@ -49,11 +32,10 @@ export default function PianoKey({octave, index,
     let bind = configuration.keyBinds[key] === undefined ? "" : configuration.keyBinds[key]
 
     let Notes = configuration.viewNotes
-    let Binds = configuration.viewBinds
+    let Binds = ""
 
     return <button
         className={checkBlack(index)}
         onClick={PlaySound}
-        onBlur={(e)=>{if(editing) e.currentTarget.classList.remove("editing")}}
     ><div>{Notes && MelodicalScale[index]}<br/>{Binds && bind}</div></button>
 }
